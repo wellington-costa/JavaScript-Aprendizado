789c9e32a1a7bcb2adf69cdbe5855ab097448c04
wellington-costa <wellington_costa@live.com>
2023-02-21 19:43:34 -0300

    initial commit

---
+++ Modulo 1/js/console-log.js
@@ -0,0 +1,13 @@
+let nome = "Wellington";
+let flo = 15.5;
+let bool = true;
+let list = [1,"2",3];
+let dict = {'1':2};
+console.log("Meu nome e ",nome,". Estou aprendendo JavaScript");
+console.log(typeof(nome));
+console.log(typeof(flo));
+console.log(typeof(bool));
+console.log(typeof(list));
+console.log(typeof dict, dict);
+
+//alert("aperte 'OK'");
