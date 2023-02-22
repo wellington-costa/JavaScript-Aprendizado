30f20e5cebcb2b134644d331118f11bc6a37cafd
wellington-costa <wellington_costa@live.com>
2023-02-21 22:06:22 -0300

    primeiro modulo

--- Modulo 1/js/console-log.js
+++ Modulo 1/js/console-log.js
@@ -1,3 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+/*
+function saudacao(){
+    console.log('Bom dia');
+}
+
+const a = function(n){return n**2};// Funçao anonima 
+const b = n=> n**2; // Arrow function 
+
+/*
+// FUNÇOES ARRAY
+const alunos = ['Maria','Laura']
+alunos.push('Carlos') // Adiciona no final
+alunos.unshift('Mariane')// Adiciona no começo
+alunos.shift()//exclui do começo
+delete alunos[1]
+alunos.pop() // Remove do final
+alunos.slice(0,-1) // fatia a lista
+
+
+
+let num = Number(prompt("Digite um numero:"));
+const numTitulo = document.getElementById("numero");
+const texto = document.getElementById("texto");
+
+numTitulo.innerHTML = num;
+texto.innerHTML=';'
+texto.innerHTML += `<p> Raiz quadrada: ${num ** 0.5} </p>`;
+texto.innerHTML += `<p> Inteiro: ${Number.isInteger(num)} </p>`;
+texto.innerHTML += `<p> NaN: ${Number.isNaN(num)} </p>`;
+texto.innerHTML += `<p> Arredonda para baixo: ${Math.floor(num)} </p>`;
+texto.innerHTML += `<p> Duas casas decimais: ${num.toFixed(2)} </p>`;
+texto.innerHTML += `<p> Arredonda para cima: ${Math.ceil(num)} </p>`;
+texto.innerHTML += `<p> Arredonda : ${Math.round(num)} </p>`;
+
+
+
+/*
 let nome = "Wellington";
 let flo = 15.5;
 let bool = true;
@@ -9,5 +57,13 @@ console.log(typeof(flo));
 console.log(typeof(bool));
 console.log(typeof(list));
 console.log(typeof dict, dict);
+console.log("");
+ Operadores aritmeticos
+   ** Potenciaçao
+   % resto de divisao
+   ++ incrementador
+   a += 2 incrementa
+   a *= 2 multiplica
+   objeto Math para operaçoes com numeros
+    */
 
-//alert("aperte 'OK'");
