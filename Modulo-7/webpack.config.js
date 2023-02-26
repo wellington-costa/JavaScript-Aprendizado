74c3712dbf71831a01e5e84b9175da4ee1882ec2
wellington-costa <wellington_costa@live.com>
2023-02-26 17:39:54 -0300

    Webpack modificado

--- Modulo-7/webpack.config.js
+++ Modulo-7/webpack.config.js
@@ -1,25 +1,26 @@
-const path = require('path');
+const path = require('path'); // CommonJS
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public', 'assets', 'js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            exclude: /node_modules/,
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/env']
-                }
-            }
-        }]
-    },
-    devtool: 'source-map',
-
-
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  devtool: 'source-map'
 };
